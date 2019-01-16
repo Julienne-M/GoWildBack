7610bcc23775576c0192b0709ab2df2b04795a17
Julienne Matias <julienne.m93@gmail.com>
2019-01-16 16:43:21 +0100

    Ajout classes Event et RessourceNotFoundException

--- src/main/java/fr/wcs/gowild/controller/EventController.java
+++ src/main/java/fr/wcs/gowild/controller/EventController.java
@@ -1,5 +1,73 @@
 package fr.wcs.gowild.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.wcs.gowild.exception.RessourceNotFoundException;
+import fr.wcs.gowild.model.Event;
+import fr.wcs.gowild.repository.EventRepository;
+
+@RestController
+@RequestMapping("/api")
 public class EventController {
+
+	@Autowired
+	EventRepository eventRepository;
+
+	// CRUD = Read
+	@GetMapping("/events")
+	public List<Event> getAllEvents() {
+		return eventRepository.findAll();
+	}
+
+	// CRUD = Read
+	@GetMapping("/events/{id}")
+	public Event getEventById(@PathVariable(value = "id") Long eventId) {
+		return eventRepository.findById(eventId).orElseThrow(() -> new RessourceNotFoundException("Event", "id", eventId));
+	}
+
+	// CRUD = Create
+	@PostMapping("/events")
+	public Event createEvent(@Valid @RequestBody Event event) {
+		return eventRepository.save(event);
+	}
+
+	// CRUD = Update
+	@PutMapping("/events/{id}")
+	public Event updateEvent(@PathVariable(value = "id") Long eventId, @RequestBody Event eventDetails) {
+		Event event = eventRepository.findById(eventId).orElseThrow(() -> new RessourceNotFoundException("Event", "id", eventId));
+		event.setTitle(eventDetails.getTitle());
+		event.setDescription(eventDetails.getDescription());
+		event.setDate_start(eventDetails.getDate_start());
+		event.setDate_end(eventDetails.getDate_end());
+		event.setTimetable(eventDetails.getTimetable());
+		event.setPricing_info(eventDetails.getPricing_info());
+		event.setLink(eventDetails.getLink());
+		event.setPlacename(eventDetails.getPlacename());
+		event.setAddress(eventDetails.getAddress());
+
+		Event updateEvent = eventRepository.save(event);
+		return updateEvent;
+	}
+
+	// CRUD = Delete
+	@DeleteMapping("/events/{id}")
+	public ResponseEntity<?> deleteEvent(@PathVariable(value = "id") Long eventId) {
+		Event event = eventRepository.findById(eventId).orElseThrow(() -> new RessourceNotFoundException("Event", "id", eventId));
+		eventRepository.delete(event);
+		return ResponseEntity.ok().build();
+	}
 
 }
--- src/main/java/fr/wcs/gowild/model/Event.java
+++ src/main/java/fr/wcs/gowild/model/Event.java
@@ -1,5 +1,151 @@
 package fr.wcs.gowild.model;
 
-public class Event {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+//import javax.persistence.*;
+//importe tout le répertoire
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//Annotation = commence toujours par "@", Entity = bean
+@Entity
+@Table(name="events")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public class Event implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	private String title;
+
+	@NotBlank
+	private String description;
+
+	@NotBlank
+	private String date_start;
+
+	private String date_end;
+
+	@NotBlank
+	private String timetable;
+
+	@NotBlank
+	private String pricing_info;
+
+	private String link;
+
+	@NotBlank
+	private String placename;
+
+	@NotBlank
+	private String address;
+
+	/*@Column(nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	private Date createdAt;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	private Date updatedAt;*/
+
+	public Event() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDate_start() {
+		return date_start;
+	}
+
+	public void setDate_start(String date_start) {
+		this.date_start = date_start;
+	}
+
+	public String getDate_end() {
+		return date_end;
+	}
+
+	public void setDate_end(String date_end) {
+		this.date_end = date_end;
+	}
+
+	public String getTimetable() {
+		return timetable;
+	}
+
+	public void setTimetable(String timetable) {
+		this.timetable = timetable;
+	}
+
+	public String getPricing_info() {
+		return pricing_info;
+	}
+
+	public void setPricing_info(String pricing_info) {
+		this.pricing_info = pricing_info;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getPlacename() {
+		return placename;
+	}
+
+	public void setPlacename(String placename) {
+		this.placename = placename;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
