9de04a1868d3ddaf825c19156d6050da8d5bb2a9
Julienne Matias <julienne.m93@gmail.com>
2019-01-18 13:47:01 +0100

    Ajout @CrossOrigin

--- src/main/java/fr/wcs/gowild/controller/EventController.java
+++ src/main/java/fr/wcs/gowild/controller/EventController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ public class EventController {
 	EventRepository eventRepository;
 
 	// CRUD = Read
+	@CrossOrigin
 	@GetMapping("/events")
 	public List<Event> getAllEvents() {
 		return eventRepository.findAll();
